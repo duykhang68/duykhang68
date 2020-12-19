3529258b6fdb874c6aaa3d488f6316a0960d13c0
Harry <duykhang6898@gmail.com>
2020-12-19 18:09:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const readme = `\
 
 ⏰ Updated on ${new Date().toUTCString()}
 
-![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
+![Progress Bar CI](https://github.com/duykhang68/duykhang68/workflows/Progress%20Bar%20CI/badge.svg)\
 `
 
 console.log(readme)
